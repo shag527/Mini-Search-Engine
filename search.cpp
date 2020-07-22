a2e4b838c5cdafa1f77fc57ec039c058663f12e6
shag527 <shagunkatoch527@gmail.com>
2020-07-22 15:09:53 +0530

    Second update

--- search.cpp
+++ search.cpp
@@ -46,6 +46,100 @@ void search(char* token, Trie *trie, Map *obj,int k)
     ceil=k;
     struct winsize w;
     ioctl(STDOUT_FILENO,TIOCGWINSZ,&w);
+    for(int l=0;l<ceil;l++)
+    {
+        int id=heap->get_id();
+        if(id==1)
+        break;
+        double score=(double)heap->remove();
+        cout<<"("<<id;
+        int x=10;
+        int f=1;
+        while(id/x!=0)
+        {
+            x*=10;
+            f++;
+        }
+        while(5-f>=0)
+        {
+            cout<<" ";
+            f++;
+        }
+        printf(")[%10.6f]",score);
+        char* line=new char[obj->get_buffer_size()];
+        strcpy(line,obj->get_document(id));
+        char *temp=strtok(line," \t\n");
+        int cur_length=0;
+        int counter=0;
+        int new_line=1;
+        int line_flag=0;
+        int underline[2][100],u_counter=0;
+        while(temp!=NULL)
+        {
+            if(new_line)
+            {
+                cur_length+=20;
+                if(counter!=0)
+                {
+                    for(int c=0;c<20;c++)
+                    {
+                        cout<<" ";
+                    }
+                }
+                new_line=0;
+            }
+
+            for(int n=0;n<i;n++)
+            {
+                if(!strcmp(arr[n],temp))
+                {
+                    if(cur_length+strlen(temp)+1<=w.ws_col)
+                    {
+                        underline[0][u_counter]=cur_length;
+                        underline[1][u_counter]=strlen(temp);
+                        u_counter++;
+                        line_flag=1;
+                    }
+                    break;
+                }
+            }
+            cur_length+=strlen(temp)+1;
+            if(cur_length-1>=w.ws_col)
+            {
+                cur_length=0;
+                new_line=1;
+                cout<<endl;
+                if(line_flag)
+                {
+                    char *output_line=new char[w.ws_col+1];
+                    for(int j=0;j<w.ws_col;j++)
+                    {
+                        output_line[j]=' ';
+                    }
+                    output_line[w.ws_col]='\0';
+                    line_flag=0;
+                    for(int j=0;j<u_counter;j++)
+                    {
+                        for(int v=underline[0][j];v<underline[0][j]+underline[1][j];v++)
+                        output_line[v]='^';
+                    }
+                    u_counter=0;
+                    cout<<output_line;
+                    delete[] output_line;
+                }
+                continue;
+            }
+            cout<<temp<<" ";
+            temp=strtok(NULL," \t\n");
+            counter++;
+        }
+        cout<<endl;
+        delete line;
+        delete temp;
+    }
+    delete heap;
+    delete scorelist;
+    cout<<endl;
 }
 void df(Trie *trie)
 {
--- search.h
+++ search.h
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<stdio.h>
 #include "ranking.h"
 #include "indexing.h"
 #include "scorelist.h"
